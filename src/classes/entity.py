d6c68a94cb7477bb309e6b72e9e9d8225db089af
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-23 14:11:16 +0100

    Add movement

--- src/classes/entity.py
+++ src/classes/entity.py
@@ -18,3 +18,26 @@ class Entity:
 
     def render(self):
         self.screen.blit(self.image, self.position)
+
+    def tick(self):
+        self.move()
+        self.render()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        distance_travelled = pygame.Vector2()
+
+        if keys[pygame.K_w]:
+            distance_travelled.y -= 1
+        if keys[pygame.K_s]:
+            distance_travelled.y += 1
+        if keys[pygame.K_a]:
+            distance_travelled.x -= 1
+        if keys[pygame.K_d]:
+            distance_travelled.x += 1
+
+        if distance_travelled.magnitude_squared() != 0:
+            distance_travelled = distance_travelled.normalize() * 225 * dt
+
+            self.position += distance_travelled
