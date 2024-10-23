c7a6c0d9670c06ad60bd5186bc303133f3b7979d
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-23 14:07:20 +0100

    Add method to render character

--- src/classes/entity.py
+++ src/classes/entity.py
@@ -2,12 +2,19 @@ import pygame
 
 class Entity:
 
+    screen = None
+
     position = None
 
     image = None
 
-    def __init__(self, position, character, size):
+    def __init__(self, screen, position, character, size):
+        self.screen = screen
+
         self.position = position
 
         image = pygame.image.load("./assets/characters/" + character + ".png").convert()
         self.image = pygame.transform.scale(image, size)
+
+    def render(self):
+        self.screen.blit(self.image, self.position)
