2f3f3df09951f14d9fcbc789ff13ee710aa6191c
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-04 20:48:08 +0000

    Create basic attacking logic for players

--- entities/living/players/player.py
+++ entities/living/players/player.py
@@ -1,5 +1,7 @@
 import pygame
+from pygame import Vector2
 
+from entities.entity import Entity
 from entities.living.living_entity import LivingEntity
 from enums.movement_keys import movement_keys
 
@@ -13,6 +15,12 @@ class Player(LivingEntity):
         self.movement_type = movement_type
 
         Player.players.append(self)
+
+    def tick(self):
+        super().tick()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            self.attack()
 
     def move(self):
         keys = pygame.key.get_pressed()
@@ -33,3 +41,29 @@ class Player(LivingEntity):
     def remove(self):
         super().remove()
         Player.players.remove(self)
+
+    def attack(self):
+        attackable_entities = []
+
+        for entity in Entity.entities:
+            if not isinstance(entity, LivingEntity):
+                continue
+
+            if isinstance(entity, Player):
+                continue
+
+            attackable_entities.append(entity)
+
+        if len(attackable_entities) == 0:
+            return
+
+        closest_entity = None
+        for entity in attackable_entities:
+            if closest_entity is None:
+                closest_entity = entity
+                continue
+
+            if Vector2(closest_entity.position).distance_squared_to(self.position) < Vector2(entity.position).distance_squared_to(self.position):
+                closest_entity = entity
+
+        self.attack_entity(closest_entity)
