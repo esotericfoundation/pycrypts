2f87defe377b2bf5457115f5b79596630dbdaebb
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-05 11:34:22 +0000

    Update health bar
    Co-authored-by: rolyPolyVole <yezhanting@gmail.com>

--- tickable/health_bar.py
+++ tickable/health_bar.py
@@ -4,6 +4,7 @@ from pygame import Vector2
 from game import Game
 
 from tickable.entities.living.living_entity import LivingEntity
+from tickable.text import Text
 from tickable.tickable import Tickable
 
 
@@ -18,9 +19,15 @@ class HealthBar(Tickable):
         self.width = width
         self.height = height
 
+        self.text = Text(str(entity.health), (top_left_x + 5, top_left_y) , (160, 0, 0), 35)
+
     def tick(self):
         self.render()
 
     def render(self):
+        self.text.text = str(self.entity.health)
+
         pygame.draw.rect(Game.screen, (115, 115, 115), (self.top_left_x - 5, self.top_left_y - 5, self.width + 10, self.height + 10))
         pygame.draw.rect(Game.screen, (200, 50, 50), (self.top_left_x, self.top_left_y, self.width * (self.entity.health / self.entity.max_health), self.height))
+
+        self.text.render()
