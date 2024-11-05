530a682bdb168f449557564144119eb797ed8a46
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-05 15:38:32 +0000

    Set the window icon

--- game.py
+++ game.py
@@ -7,6 +7,9 @@ class Game:
     screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
     pygame.display.set_caption("Dungeon Crawler")
 
+    icon = pygame.image.load('assets/big-skeleton-face.png')
+    pygame.display.set_icon(icon)
+
     height = screen.get_height()
     width = screen.get_width()
 
