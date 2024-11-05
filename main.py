135a25d7894b346ab2fdb3b108f2df2c02bf88a3
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-05 10:47:01 +0000

    Create basic health bar system
    Co-authored-by: rolyPolyVole <yezhanting@gmail.com>

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tickable.tickable import Tickable
 from tickable.entities.living.players.player import Player
 from enums.movement_keys import movement_keys
 from game import Game
+from tickable.health_bar import HealthBar
 
 running = True
 
@@ -23,6 +24,8 @@ def tick():
 
 player = Player((Game.screen.get_width() / 2 + 100, Game.screen.get_height() / 2), "pro", 64, movement_keys["ARROW"], pygame.K_RSHIFT)
 rizzler = Player((Game.screen.get_width() / 2 - 100, Game.screen.get_height() / 2), "rizzler", 64, movement_keys["WASD"], pygame.K_LSHIFT)
+
+player_health_bar = HealthBar(player, 100, 600, 300, 40)
 
 monster = Skeleton((Game.screen.get_width() / 2, Game.screen.get_height() / 2 - 100), 64)
 
---
+++ tickable/health_bar.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame import Vector2
+
+from game import Game
+
+from tickable.entities.living.living_entity import LivingEntity
+from tickable.tickable import Tickable
+
+
+class HealthBar(Tickable):
+
+    def __init__(self, entity: LivingEntity, top_left_x: int, top_left_y: int, width: int, height: int):
+        super().__init__()
+        self.entity = entity
+
+        self.top_left_x = top_left_x
+        self.top_left_y = top_left_y
+        self.width = width
+        self.height = height
+
+    def tick(self):
+        self.render()
+
+    def render(self):
+        pygame.draw.rect(Game.screen, (200, 20, 20), (self.top_left_x, self.top_left_y, self.width * (self.entity.health / self.entity.max_health), self.height))
