ae3e3471a976ee36672902fcd2494094e8e856ae
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-05 15:36:14 +0000

    Set window name

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ pygame.init()
 
 class Game:
     screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
+    pygame.display.set_caption("Dungeon Crawler")
 
     height = screen.get_height()
     width = screen.get_width()
