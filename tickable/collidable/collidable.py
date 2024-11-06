61a68c1021a3d016030e52a5763cbe8adec7e3c6
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-06 12:15:17 +0000

    Update collision system (attempt 2)

--- tickable/collidable/collidable.py
+++ tickable/collidable/collidable.py
@@ -2,11 +2,20 @@ from tickable.tickable import Tickable
 
 
 class Collidable(Tickable):
+
+    collidables: list["Collidable"] = []
+
     def __init__(self):
         super().__init__()
+        Collidable.collidables.append(self)
 
     def is_inside_hitbox(self, location: tuple[int, int]) -> bool:
         pass
 
-    def is_colliding(self, entity) -> bool:
+    def is_colliding(self, collidable: "Collidable") -> bool:
         pass
+
+    def remove(self):
+        super().remove()
+
+        Collidable.collidables.remove(self)
--- tickable/collidable/entities/entity.py
+++ tickable/collidable/entities/entity.py
@@ -35,19 +35,21 @@ class Entity(Collidable):
 
             self.position += distance_travelled
 
-            for entity in Entity.entities:
+            for entity in Collidable.collidables:
                 if entity == self:
                     continue
 
-                if self.is_colliding(entity):
+                if self.is_colliding(entity) or entity.is_colliding(self):
                     self.position -= distance_travelled
                     break
 
     def is_inside_hitbox(self, location: tuple[int, int]) -> bool:
         return self.position.distance_to(location) < (self.size / 2)
 
-    def is_colliding(self, entity) -> bool:
-        return self.position.distance_to(entity.position) < (self.size / 2 + entity.size / 2)
+    def is_colliding(self, entity: Collidable) -> bool:
+        if isinstance(entity, Entity):
+            return self.position.distance_to(entity.position) < (self.size / 2 + entity.size / 2)
+        return False # Collisions with things other than entities will be managed by those classes themselves.
 
     def remove(self):
         super().remove()
