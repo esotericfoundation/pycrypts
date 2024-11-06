227e53551a0d34efd3019fb3a051db42d4712111
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-06 11:10:50 +0000

    Make tutorial wall thinner

--- rooms/tutorial.py
+++ rooms/tutorial.py
@@ -5,4 +5,4 @@ from rooms.wall import Wall
 
 class Tutorial(Room):
     def __init__(self):
-        super().__init__([Wall(Game.top_left, Game.bottom_left + (100, 0))])
+        super().__init__([Wall(Game.top_left, Game.bottom_left + (50, 0))])
