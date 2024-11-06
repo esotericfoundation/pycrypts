40c6c25248b1d15cc85594496613a5a6bb44cc17
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-06 10:58:12 +0000

    Add start of tutorial room

---
+++ rooms/tutorial.py
@@ -0,0 +1,8 @@
+from game import Game
+from rooms.room import Room
+from rooms.wall import Wall
+
+
+class Tutorial(Room):
+    def __init__(self):
+        super().__init__(Wall(Game.top_left + (100, 0), Game.bottom_left))
