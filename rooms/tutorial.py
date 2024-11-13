52456efe2813888fa56d288cb63aee529b80c8d6
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-13 09:52:27 +0000

    Add expected values

--- rooms/tutorial.py
+++ rooms/tutorial.py
@@ -6,3 +6,5 @@ from tickable.collidable.walls.wall import Wall
 class Tutorial(Room):
     def __init__(self):
         super().__init__([Wall(Game.center - (50, 50), Game.center + (50, 50))])
+        # Width = 100
+        # Height = 100
