94ce0cf95a490a4c9f41d80bd26ef0ced28befec
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-20 22:00:58 +0000

    Add load method

--- tickable/tickable.py
+++ tickable/tickable.py
@@ -3,10 +3,13 @@ class Tickable:
     tickables: list["Tickable"] = []
 
     def __init__(self):
-        Tickable.tickables.append(self)
+        self.load()
 
     def tick(self):
         pass
+
+    def load(self):
+        Tickable.tickables.append(self)
 
     def unload(self):
         Tickable.tickables.remove(self)
