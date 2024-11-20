7ded44042aee2e045374990e0d5850e60cbfccef
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-20 14:39:17 +0000

    Spawn le skeletons!
    Co-authored-by: rolyPolyVole <yezhanting@gmail.com>

--- rooms/entrance_zone.py
+++ rooms/entrance_zone.py
@@ -1,5 +1,6 @@
 from game import Game
 from rooms.room import Room
+from tickable.collidable.entities.living.monsters.skeleton import Skeleton
 from tickable.collidable.walls.door import Door
 from tickable.collidable.walls.wall import Wall
 
@@ -27,3 +28,17 @@ class EntranceZone(Room):
 
         super().__init__([border_left, border_right_1, border_right_2, border_top_1, border_top_2, border_bottom, wall_1, wall_2, wall_3, wall_4], [entrance_door, exit_door], spawn_1, spawn_2, 0.5)
         pass
+
+    def spawn_monsters(self):
+        skeleton_1 = Skeleton((404, 410), 32)
+        skeleton_2 = Skeleton((314, 402), 32)
+        skeleton_3 = Skeleton((173, 405), 32)
+        skeleton_4 = Skeleton((423, 271), 32)
+        skeleton_5 = Skeleton((308, 266), 32)
+        skeleton_6 = Skeleton((171, 269), 32)
+        skeleton_7 = Skeleton((409, 178), 32)
+        skeleton_8 = Skeleton((261, 175), 32)
+        skeleton_9 = Skeleton((147, 167), 32)
+
+        self.monsters.extend((skeleton_1, skeleton_2, skeleton_3, skeleton_4, skeleton_5, skeleton_6, skeleton_7, skeleton_8, skeleton_9))
+
