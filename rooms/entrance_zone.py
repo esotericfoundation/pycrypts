690cb2ca4dfc907c0d1de628bcc60575396d746c
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-20 13:42:07 +0000

    Start from a clean slate

--- rooms/entrance_zone.py
+++ rooms/entrance_zone.py
@@ -14,14 +14,11 @@ class EntranceZone(Room):
         border_top_2 = Wall(Game.top_left + (400, 0), Game.top_right + (0, 80))
         border_bottom = Wall(Game.bottom_left + (0, -80), Game.bottom_right)
 
-        wall_1 = Wall(Game.top_left + (480, 0), Game.top_left + (560, 160))
-        wall_2 = Wall(Game.top_left + (480, 320), Game.bottom_left + (560, 0))
-
         spawn_1 = (Game.top_right + (-200, 240))
         spawn_2 = (Game.bottom_right + (-200, -320))
 
         entrance_door = Door(border_right_1.bottom_right - (border_right_1.get_width(), 0), border_right_2.top_left + (border_right_2.get_width(), 0), None)
         exit_door = Door(border_top_1.top_left + (border_top_1.get_width(), 0), border_top_2.bottom_right - (border_top_2.get_width(), 0), None)
 
-        super().__init__([border_left, border_right_1, border_right_2, border_top_1, border_top_2, border_bottom, wall_1, wall_2], [entrance_door, exit_door], spawn_1, spawn_2)
+        super().__init__([border_left, border_right_1, border_right_2, border_top_1, border_top_2, border_bottom], [entrance_door, exit_door], spawn_1, spawn_2)
         pass
