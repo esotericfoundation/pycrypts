1159aed999ce7f6d3bfbc37db3d96a35b598d362
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-24 16:54:39 +0000

    Move walls to the right

--- rooms/entrance_zone.py
+++ rooms/entrance_zone.py
@@ -13,13 +13,13 @@ class EntranceZone(Room):
         border_top_2 = Wall(game.top_left + (400, 0), game.top_right + (0, 40), game)
         border_bottom = Wall(game.bottom_left + (0, -40), game.bottom_right, game)
 
-        spawn_1 = (game.top_right + (-200, 240))
-        spawn_2 = (game.bottom_right + (-200, -320))
+        spawn_1 = (game.top_right + (-100, 240))
+        spawn_2 = (game.bottom_right + (-100, -320))
 
         wall_1 = Wall([400, 500], [1250, 550], game)
-        wall_2 = Wall([1000, 200], [1050, 500], game)
-        wall_3 = Wall([775, 25], [825, 325], game)
-        wall_4 = Wall([550, 200], [600, 500], game)
+        wall_2 = Wall([1050, 200], [1100, 500], game)
+        wall_3 = Wall([850, 25], [900, 325], game)
+        wall_4 = Wall([650, 200], [700, 500], game)
 
         entrance_door = Door(border_right_1.bottom_right - (border_right_1.get_width(), 0), border_right_2.top_left + (border_right_2.get_width(), 0), None, game)
         exit_door = Door(border_top_1.top_left + (border_top_1.get_width(), 0), border_top_2.bottom_right - (border_top_2.get_width(), 0), None, game)
