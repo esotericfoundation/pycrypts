46992b8e97c86bc687a8bbd985035fa957eecd9a
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-24 17:02:57 +0000

    Remove exit for now

--- rooms/entrance_zone.py
+++ rooms/entrance_zone.py
@@ -9,8 +9,7 @@ class EntranceZone(Room):
         border_left = Wall(game.top_left, game.bottom_left + (40, 0), game)
         border_right_1 = Wall(game.top_right + (-40, 0), game.top_right + (0, 240), game)
         border_right_2 = Wall(game.top_right + (-40, 480), game.bottom_right, game)
-        border_top_1 = Wall(game.top_left, game.top_left + (160, 40), game)
-        border_top_2 = Wall(game.top_left + (400, 0), game.top_right + (0, 40), game)
+        border_top = Wall(game.top_left, game.top_right + (0, 40), game)
         border_bottom = Wall(game.bottom_left + (0, -40), game.bottom_right, game)
 
         spawn_1 = (game.top_right + (-100, 240))
@@ -22,9 +21,8 @@ class EntranceZone(Room):
         wall_4 = Wall([650, 200], [700, 500], game)
 
         entrance_door = Door(border_right_1.bottom_right - (border_right_1.get_width(), 0), border_right_2.top_left + (border_right_2.get_width(), 0), None, game)
-        exit_door = Door(border_top_1.top_left + (border_top_1.get_width(), 0), border_top_2.bottom_right - (border_top_2.get_width(), 0), None, game)
 
-        super().__init__([border_left, border_right_1, border_right_2, border_top_1, border_top_2, border_bottom, wall_1, wall_2, wall_3, wall_4], [entrance_door, exit_door], spawn_1, spawn_2, game, 0.5)
+        super().__init__([border_left, border_right_1, border_right_2, border_top, border_bottom, wall_1, wall_2, wall_3, wall_4], [entrance_door], spawn_1, spawn_2, game, 0.5)
         pass
 
     def spawn_monsters(self):
