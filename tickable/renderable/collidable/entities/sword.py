35cf16df6fb2b52a371c95dcc83302d21a602f76
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-25 22:21:30 +0000

    Rotate the sword

--- tickable/renderable/collidable/entities/sword.py
+++ tickable/renderable/collidable/entities/sword.py
@@ -1,3 +1,7 @@
+import math
+
+import pygame
+
 from tickable.renderable.collidable.collidable import Collidable
 from tickable.renderable.collidable.entities.entity import Entity
 
@@ -7,10 +11,11 @@ class Sword(Entity):
     def __init__(self, target, position, game):
         super().__init__(position, "sword", 64, game)
 
-        self.position = self.get_center()
-
         self.target = target
         self.time_left = 0.5
+
+        target_angle = 360 - math.degrees((self.target.get_center() - self.position).as_polar()[1])
+        self.image = pygame.transform.rotate(self.image, target_angle)
 
     def tick(self):
         super().tick()
