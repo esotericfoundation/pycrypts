84cc3ddd8b5a16d9560c0eccb470a2b13ff4c5bd
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-25 19:54:14 +0000

    Make swords a constant size

--- tickable/renderable/collidable/entities/sword.py
+++ tickable/renderable/collidable/entities/sword.py
@@ -4,8 +4,8 @@ from tickable.renderable.collidable.entities.entity import Entity
 
 class Sword(Entity):
 
-    def __init__(self, target, position, size, game):
-        super().__init__(position, "sword", size, game)
+    def __init__(self, target, position, game):
+        super().__init__(position, "sword", 64, game)
         self.target = target
         self.time_left = 0.5
 
