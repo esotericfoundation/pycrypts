5fa53b460cbcee15472921dd05ecc252a86b2b58
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-25 19:52:25 +0000

    Make swords disappear promptly

--- tickable/renderable/collidable/entities/sword.py
+++ tickable/renderable/collidable/entities/sword.py
@@ -1,3 +1,4 @@
+from tickable.renderable.collidable.collidable import Collidable
 from tickable.renderable.collidable.entities.entity import Entity
 
 
@@ -6,3 +7,17 @@ class Sword(Entity):
     def __init__(self, target, position, size, game):
         super().__init__(position, "sword", size, game)
         self.target = target
+        self.time_left = 0.5
+
+    def tick(self):
+        super().tick()
+        self.time_left -= self.game.dt
+
+        if self.time_left <= 0:
+            self.unload()
+
+    def is_colliding(self, entity: Collidable) -> bool:
+        return False
+
+    def is_hitting(self, entity: Collidable) -> bool:
+        return super().is_colliding(entity)
