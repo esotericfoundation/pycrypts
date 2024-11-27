58f84da271f431ca5dc3fb4e8545f177c8bc894e
rolyPolyVole <yezhanting@gmail.com>
2024-11-27 19:54:38 +0000

    Add back off goal

---
+++ tickable/renderable/collidable/entities/living/monsters/ai/goals/back_off_from_target.py
@@ -0,0 +1,20 @@
+from tickable.renderable.collidable.entities.living.monsters.ai.goals.walk_to_target import WalkToTargetGoal
+
+
+class BackOffFromTargetGoal(WalkToTargetGoal):
+    def __init__(self, owner, priority, game, speed = 1, distance_threshold = 100):
+        super().__init__(owner, priority, game, speed)
+
+        self.distance_threshold = distance_threshold
+
+    def tick(self):
+        self.owner.move_away_from(self.cached_target, self.speed)
+
+    def can_use(self) -> bool:
+        return super().can_use() and len(self.get_nearby_targets_and_cache()) > 0
+
+    def get_nearby_targets_and_cache(self):
+        targets = super().get_nearby_targets_and_cache()
+        nearby = list(filter(lambda p: self.owner.position.distance_squared_to(p.position) < self.distance_threshold * self.distance_threshold, targets))
+
+        return nearby
