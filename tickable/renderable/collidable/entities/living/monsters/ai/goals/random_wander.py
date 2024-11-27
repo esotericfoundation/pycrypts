800aa981e4f9d206522a8cb0ee9ae57d971a69cf
rolyPolyVole <yezhanting@gmail.com>
2024-11-27 19:54:47 +0000

    Add wander goal

---
+++ tickable/renderable/collidable/entities/living/monsters/ai/goals/random_wander.py
@@ -0,0 +1,51 @@
+import random
+
+from pygame import Vector2
+
+from tickable.renderable.collidable.entities.living.monsters.ai.goal import Goal
+
+class RandomWanderGoal(Goal):
+    def __init__(self, owner, priority, game, speed = 1, wander_duration = 1, wander_cooldown = 1, randomness = 0.35):
+        super().__init__(owner, priority, game)
+
+        self.speed = speed
+        self.wander_duration = wander_duration
+        self.wander_cooldown = wander_cooldown
+        self.randomness = randomness
+
+        self.wander_time = 0
+        self.idle_time = 0
+
+        self.wander_direction = None
+        self.wandering = False
+
+    def start(self):
+        self.start_wandering()
+
+    def tick(self):
+        if self.wandering:
+            self.owner.move_without_collision(self.wander_direction, 0.35)
+            self.wander_time += self.game.dt
+
+            if self.wander_time >= self.wander_duration + random.uniform(-self.randomness, self.randomness):
+                self.stop_wandering()
+        else:
+            self.idle_time += self.game.dt
+
+            if self.idle_time >= self.wander_cooldown + random.uniform(-self.randomness, self.randomness):
+                self.start_wandering()
+
+    def end(self):
+        self.stop_wandering()
+
+    def can_use(self) -> bool:
+        return super().can_use()
+
+    def start_wandering(self):
+        self.wander_direction = Vector2(random.uniform(-1, 1), random.uniform(-1, 1))
+        self.wandering = True
+        self.idle_time = 0
+
+    def stop_wandering(self):
+        self.wandering = False
+        self.wander_time = 0
