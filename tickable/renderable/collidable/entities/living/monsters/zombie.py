7cc2685164608d714feb348f49807073e554d202
rolyPolyVole <yezhanting@gmail.com>
2024-11-27 00:57:33 +0000

    Add idle behaviour

--- tickable/renderable/collidable/entities/living/monsters/zombie.py
+++ tickable/renderable/collidable/entities/living/monsters/zombie.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame import Vector2
 
@@ -7,15 +9,39 @@ from tickable.renderable.collidable.entities.living.players.player import get_pl
 
 
 class Zombie(Monster):
+        wander_cooldown = 1.0
+        wander_duration = 1.5
+
         def __init__(self, position: tuple[int, int], size: int, game: "Game"):
             super().__init__(position, "zombie", size, 80, game)
 
+            self.wander_direction = Vector2(0, 0)
+            self.wander_time = 0
+            self.idle_time = 0
+            self.wandering = False
+
         def ai_tick(self):
             players = list(sorted(list(filter(lambda p: self.sees_other(p), get_players())), key=lambda p: self.position.distance_squared_to(p.position)))
+
             if len(players) > 0:
                 player = players[0]
-                self.move_towards(player, 0.5)
+                self.move_towards(player, 0.65)
+                self.wandering = False
             else:
+                if self.wandering:
+                    self.move_without_collision(self.wander_direction, 0.35)
+                    self.wander_time += self.game.dt
+
+                    if self.wander_time >= Zombie.wander_duration:
+                        self.wandering = False
+                        self.wander_time = 0
+                else:
+                    self.idle_time += self.game.dt
+
+                    if self.idle_time >= Zombie.wander_cooldown:
+                        self.wander_direction = Vector2(random.uniform(-1, 1), random.uniform(-1, 1))
+                        self.wandering = True
+                        self.idle_time = 0
 
         def attack_entity(self, entity: "LivingEntity"):
             if self.position.distance_squared_to(entity.position) < 10000:
