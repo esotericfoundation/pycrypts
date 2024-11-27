86a15613d1b35f1fa796b58fd21a6c29af2b1a7f
rolyPolyVole <yezhanting@gmail.com>
2024-11-27 19:58:03 +0000

    Refactor rooms system

--- rooms/entrance_zone.py
+++ rooms/entrance_zone.py
@@ -1,3 +1,5 @@
+from pygame import Vector2
+
 from rooms.room import Room
 from tickable.renderable.collidable.entities.living.monsters.skeleton import Skeleton
 from tickable.renderable.collidable.walls.brittle_wall import BrittleWall
@@ -9,15 +11,21 @@ class EntranceZone(Room):
     entity_scale = 0.5
 
     def __init__(self, game: "Game"):
-        self.game = game
-        self.skeletons = []
-
-        spawn_1 = (game.top_right + (-100, 240))
-        spawn_2 = (game.bottom_right + (-100, -320))
+        spawn_1 = Vector2(game.top_right + (-100, 240))
+        spawn_2 = Vector2(game.bottom_right + (-100, -320))
 
         super().__init__(spawn_1, spawn_2, game, EntranceZone.entity_scale)
 
+        self.skeletons = []
+
     def create(self):
+        if self.created:
+            return
+
+        super().create()
+
+        self.spawn_monsters()
+
         border_left = Wall(self.game.top_left, self.game.bottom_left + (40, 0), self.game)
         border_right_1 = Wall(self.game.top_right + (-40, 0), self.game.top_right + (0, 240), self.game)
         border_right_2 = Wall(self.game.top_right + (-40, 480), self.game.bottom_right, self.game)
@@ -28,7 +36,23 @@ class EntranceZone(Room):
         wall_2 = Wall((1050, 200), (1100, 500), self.game)
         wall_3 = Wall((850, 25), (900, 325), self.game)
         wall_4 = Wall((650, 200), (700, 500), self.game)
+        wall_5 = BrittleWall([200, 40], [250, 550], list(self.skeletons), self.game)
 
+        entrance_door = Door(
+            self.game.top_right + (-40, 240),
+            self.game.top_right + (0, 480),
+            self.game.surface_zone,
+            (Vector2(150, 350), Vector2(250, 350)),
+            self.game)
+
+        self.walls.extend([border_left, border_right_1, border_right_2, border_top, border_bottom, wall_1, wall_2, wall_3, wall_4, wall_5])
+        self.doors.append(entrance_door)
+
+    def unload(self):
+        super().unload()
+        print("Unloading entrance zone")
+
+    def spawn_monsters(self):
         skeleton_1 = Skeleton((525, 400), 64, self.game)
         skeleton_2 = Skeleton((425, 400), 64, self.game)
         skeleton_3 = Skeleton((325, 400), 64, self.game)
@@ -40,23 +64,8 @@ class EntranceZone(Room):
         skeleton_9 = Skeleton((325, 200), 64, self.game)
 
         self.skeletons.extend([skeleton_1, skeleton_2, skeleton_3, skeleton_4, skeleton_5, skeleton_6, skeleton_7, skeleton_8, skeleton_9])
-
-        for skeleton in self.skeletons:
-            skeleton.unload()
-
-        wall_5 = BrittleWall([200, 40], [250, 550], list(self.skeletons), self.game)
-
-        entrance_door = Door(
-            border_right_1.bottom_right - (border_right_1.get_width(), 0),
-            border_right_2.top_left + (border_right_2.get_width(), 0),
-            self.game.surface_zone, self.game)
-
-        self.walls.extend([border_left, border_right_1, border_right_2, border_top, border_bottom, wall_1, wall_2, wall_3, wall_4, wall_5])
-        self.doors.append(entrance_door)
-
-    def spawn_monsters(self):
         self.monsters.extend(self.skeletons)
 
         for skeleton in self.skeletons:
-            skeleton.load()
+            skeleton.unload()
 
--- rooms/surface_zone.py
+++ rooms/surface_zone.py
@@ -5,16 +5,22 @@ from tickable.renderable.collidable.entities.living.monsters.zombie import Zombi
 from tickable.renderable.collidable.walls.wall import Wall
 from tickable.renderable.collidable.walls.door import Door
 
+
 class SurfaceZone(Room):
     def __init__(self, game: "Game"):
-        self.game = game
-
         spawn_1 = (game.top_right + (-200, 240))
         spawn_2 = (game.bottom_right + (-200, -320))
 
         super().__init__(spawn_1, spawn_2, game)
 
     def create(self):
+        if self.created:
+            return
+
+        super().create()
+
+        self.spawn_monsters()
+
         border_left = Wall(self.game.top_left, self.game.bottom_left + (80, 0), self.game)
         border_right_1 = Wall(self.game.top_right + (-80, 0), self.game.top_right + (0, 240), self.game)
         border_right_2 = Wall(self.game.top_right + (-80, 480), self.game.bottom_right, self.game)
@@ -26,20 +32,27 @@ class SurfaceZone(Room):
         wall_2 = Wall(self.game.top_left + (480, 320), self.game.bottom_left + (560, 0), self.game)
 
         entrance_door = Door(
-            border_right_1.bottom_right - (border_right_1.get_width(), 0),
-            border_right_2.top_left + (border_right_2.get_width(), 0),
-            None, self.game)
+            self.game.top_right + (-80, 240),
+            self.game.top_right + (0, 480),
+            None, None, self.game)
+
         exit_door = Door(
-            border_top_1.top_left + (border_top_1.get_width(), 0),
-            border_top_2.bottom_right - (border_top_2.get_width(), 0),
-            self.game.entrance_zone, self.game)
+            self.game.top_left + (160, 0),
+            self.game.top_left + (400, 80),
+            self.game.entrance_zone,
+            (self.game.entrance_zone.spawn_1, self.game.entrance_zone.spawn_2),
+            self.game)
 
         self.walls.extend([border_left, border_right_1, border_right_2, border_top_1, border_top_2, border_bottom, wall_1, wall_2])
         self.doors.extend([entrance_door, exit_door])
 
     def spawn_monsters(self):
-        skeleton_1 = Zombie(self.game.bottom_left + (160, -200), 64, self.game)
-        skeleton_2 = Skeleton(self.game.bottom_left + (300, -200), 64, self.game)
+        skeleton_1 = Skeleton(self.game.bottom_left + (160, -200), 64, self.game)
+        skeleton_2 = Zombie(self.game.bottom_left + (300, -200), 64, self.game)
+
+        skeleton_1.unload()
+        skeleton_2.unload()
 
         self.monsters.append(skeleton_1)
         self.monsters.append(skeleton_2)
+
