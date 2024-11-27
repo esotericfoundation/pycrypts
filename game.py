33ba7b8294ca11d0fe369bf2c2a52120c21ac1bb
rolyPolyVole <yezhanting@gmail.com>
2024-11-27 19:59:23 +0000

    Add game end logic, save each room to the game object

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 import time
 
 import pygame
-from pygame import Vector2
+from pygame import Vector2, Surface
 
 from enums.movement_keys import movement_keys
 from rooms.entrance_zone import EntranceZone
 from rooms.surface_zone import SurfaceZone
-from tickable.renderable.collidable.entities.living.players.player import Player
+from tickable.renderable.collidable.entities.living.players.player import Player, get_players
+from tickable.renderable.collidable.walls.door import Door
 from tickable.renderable.collidable.walls.wall import Wall
 from tickable.renderable.display.health_bar import HealthBar
 from tickable.tickable import Tickable
@@ -23,7 +24,7 @@ def load_icon(window_name: str, icon_name: str):
 
 class Game:
     def __init__(self):
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen: Surface = pygame.display.set_mode((1280, 720))
         load_icon("Dungeon Crawler", "big-skeleton-face")
 
         self.debug = True
@@ -42,8 +43,10 @@ class Game:
         self.center = Vector2(self.width / 2, self.height / 2)
 
         self.current_room = None
-        self.entrance_zone = EntranceZone(self)
-        self.surface_zone = SurfaceZone(self)
+        self.entrance_zone = None
+        self.surface_zone = None
+
+        self.over = False
 
     def init(self):
         rizzler = Player((0, 0), "rizzler", 64, movement_keys["WASD"], pygame.K_LSHIFT, self)
@@ -52,13 +55,27 @@ class Game:
         HealthBar(rizzler, (self.screen.get_width() - 100 - 300, self.screen.get_height() - 140), 300, 40, self)
         HealthBar(player, (100, self.screen.get_height() - 140), 300, 40, self)
 
-        # surface_zone = SurfaceZone(self)
+        self.current_room = self.surface_zone = SurfaceZone(self)
+        self.entrance_zone = EntranceZone(self)
+
         self.surface_zone.load()
 
+        players = get_players()
+        i = 0
+
+        for player in players:
+            i += 1
+            if i == 1:
+                player.position = self.current_room.spawn_1
+            else:
+                player.position = self.current_room.spawn_2
+            player.set_scale(self.current_room.entity_scale)
+
     def tick(self):
-        present = time.time()
-        self.dt = present - self.past
-        self.past = present
+        if not self.over:
+            present = time.time()
+            self.dt = present - self.past
+            self.past = present
 
         keys = pygame.key.get_pressed()
 
@@ -69,20 +86,31 @@ class Game:
             if event.type == pygame.QUIT:
                 return False
 
-            if self.debug and event.type == pygame.MOUSEBUTTONUP:
-                self.handle_debug_mouse_click()
-
         self.screen.fill((0, 0, 0))
 
-        for tickable in Tickable.tickables:
-            tickable.tick()
+        if self.over:
+            font_1 = pygame.font.Font(None, 150)
+
+            text_1 = font_1.render("Game Over!", True, (255, 0, 0))
+            text_1_rect = text_1.get_rect(center=self.center)
+
+            self.screen.blit(text_1, text_1_rect)
+
+            font_2 = pygame.font.Font(None, 50)
+
+            text_2 = font_2.render("Press ESC to exit", True, (200, 0, 0))
+            text_2_rect = text_2.get_rect(center=(self.center.x, self.center.y + 100))
+
+            self.screen.blit(text_2, text_2_rect)
+
+        if not self.over:
+            for tickable in Tickable.tickables:
+                tickable.tick()
 
         pygame.display.flip()
         return True
 
     def handle_debug_mouse_click(self):
-        print("DEBUG MOUSE CLICK!")
-
         pos = pygame.mouse.get_pos()
         print(f'POSITION SELECTED: ({pos[0]}, {pos[1]})')
 
@@ -92,7 +120,7 @@ class Game:
         debug_pos_2 = self.clicked_positions[1]
 
         if debug_pos_1 is None:
-            self.clicked_positions[0] = pos
+            self.clicked_positions[0] = Vector2(pos)
             return
 
         if debug_pos_2 is None:
@@ -106,3 +134,10 @@ class Game:
         print(wall.to_string())
 
         self.clicked_positions = [None, None]
+
+    def end(self):
+        self.over = True
+        self.current_room.unload()
+
+        for tickable in Tickable.tickables:
+            tickable.unload()
