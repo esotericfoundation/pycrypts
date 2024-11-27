014d72268930f6aca0ab46f7b7aa0ce618df6716
rolyPolyVole <yezhanting@gmail.com>
2024-11-27 19:54:55 +0000

    Add chase target goal

---
+++ tickable/renderable/collidable/entities/living/monsters/ai/goals/walk_to_target.py
@@ -0,0 +1,34 @@
+from tickable.renderable.collidable.entities.living.monsters.ai.goal import Goal
+from tickable.renderable.collidable.entities.living.players.player import get_players
+
+
+class WalkToTargetGoal(Goal):
+    def __init__(self, owner, priority, game, speed = 1):
+        super().__init__(owner, priority, game)
+
+        self.speed = speed
+        self.cached_target = None
+
+    def start(self):
+        pass
+
+    def tick(self):
+        if self.owner.velocity.magnitude() > 0:
+            return
+
+        self.owner.move_towards(self.cached_target, self.speed)
+
+    def end(self):
+        self.cached_target = None
+        pass
+
+    def can_use(self) -> bool:
+        return super().can_use() and len(self.get_nearby_targets_and_cache()) > 0
+
+    def get_nearby_targets_and_cache(self):
+        targets = list(sorted(list(filter(lambda p: self.owner.sees_other(p), get_players())), key=lambda p: self.owner.position.distance_squared_to(p.position)))
+
+        if len(targets) > 0:
+            self.cached_target = targets[0]
+
+        return targets
