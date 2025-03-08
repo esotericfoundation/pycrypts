51bdf0424beff2f8d8023393852fabf353da24e8
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-08 17:13:17 +0000

    Figure out run configurations and building

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Builds this project into a Python package.
+# Builds this project into an executable.
 
 cd "$(dirname "$0")/.." || exit
-
-python3 -m build
 
 pyinstaller -y --onefile --add-data "./pycrypt/assets:pycrypt/assets" --name pycrypt ./pycrypt/__main__.py
 
