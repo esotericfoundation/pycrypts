be90435c0e09617c71e3485e0950517be16ca4c9
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-25 22:55:05 +0000

    Use early return in loop

--- pycrypts/game.py
+++ pycrypts/game.py
@@ -160,6 +160,7 @@ class PyCrypts:
                 for wall in self.current_room.get_walls():
                     if wall.contains_point(position):
                         self.logger.debug(f"Clicked on {wall.__class__.__name__} at {position}")
+                        break
 
         self.screen.fill((45, 45, 45))
 
