6cb4c473a40ae2834144679f1c6b7127fcd3fb7d
rolyPolyVole <yezhanting@gmail.com>
2025-03-29 21:52:46 +0000

    Fix room elements being off
    Co-authored-by: Esoteric Enderman <esotericenderman@gmail.com>

--- pycrypts/rooms/bozo_boss_barrack.py
+++ pycrypts/rooms/bozo_boss_barrack.py
@@ -21,25 +21,25 @@ class BozoBossBarrack(Room):
     def create(self):
         super().create()
 
-        top_border = Wall(self.game.top_left, self.game.top_right + (0, 50), self.game, self, True)
-        bottom_border = Wall(self.game.bottom_left + (0, -50), self.game.bottom_right, self.game, self, True)
+        top_border = Wall(self.game.top_left, self.game.top_right + (0, 40), self.game, self, True)
+        bottom_border = Wall(self.game.bottom_left + (0, -40), self.game.bottom_right, self.game, self, True)
 
-        Wall(top_border.get_bottom_left(), bottom_border.top_left + (50, 0), self.game, self, True)
-        Wall(top_border.bottom_right + (-50, 0), bottom_border.get_top_right() + (-50, 0), self.game, self, True)
+        Wall(top_border.get_bottom_left(), bottom_border.top_left + (40, 0), self.game, self, True)
+        Wall(top_border.bottom_right + (-40, 0), bottom_border.get_top_right() + (-50, 0), self.game, self, True)
 
         bozo_boss_barracks_barricade = Door((0, 280), (40, 440), self.game.entrance_zone, Vector2(80, 360), self.game, self)
 
-        Wall((40, 40), (80, 80), self.game, self, True)
-        Wall((440, 40), (480, 80), self.game, self, True)
-        Wall((360, 40), (400, 80), self.game, self, True)
-        Wall(self.game.top_right + (-80, 40), self.game.top_right + (-40, 80), self.game, self, True)
-        Wall((40, 640), (80, 680), self.game, self, True)
-        Wall((440, 640), (480, 680), self.game, self, True)
-        Wall((360, 640), (400, 680), self.game, self, True)
-        Wall(self.game.top_right + (-80, 640), self.game.top_right + (-40, 680), self.game, self, True)
+        top_left_corner_block = Wall((40, 40), (80, 80), self.game, self, True)
+        next_top_corner = Wall(top_left_corner_block.top_left + (320, 0), top_left_corner_block.bottom_right + (320, 0), self.game, self, True)
+        Wall(top_left_corner_block.top_left + (400, 0), top_left_corner_block.bottom_right + (400, 0), self.game, self, True)
+        Wall(top_left_corner_block.top_left + (1200, 0), top_left_corner_block.bottom_right + (1200, 0), self.game, self, True)
+        bottom_left_corner_block = Wall((40, 640), (80, 680), self.game, self, True)
+        next_bottom_corner = Wall(bottom_left_corner_block.top_left + (320, 0), bottom_left_corner_block.bottom_right + (320, 0), self.game, self, True)
+        Wall(bottom_left_corner_block.top_left + (400, 0), bottom_left_corner_block.bottom_right + (400, 0), self.game, self, True)
+        Wall(bottom_left_corner_block.top_left + (1200, 0), bottom_left_corner_block.bottom_right + (1200, 0), self.game, self, True)
 
-        stub_1 = Wall((480, 40), (560, 120), self.game, self, True)
-        stub_2 = Wall((480, 600), (560, 680), self.game, self, True)
+        stub_1 = Wall(next_top_corner.top_left + (40, 0), next_top_corner.bottom_right + (80, 40), self.game, self, True)
+        stub_2 = Wall(next_bottom_corner.top_left + (40, -40), next_bottom_corner.bottom_right + (80, 0), self.game, self, True)
 
         guard_1 = Zombie(bozo_boss_barracks_barricade.top_left + (120, -80), 64, self.game, self)
         guard_2 = Zombie(bozo_boss_barracks_barricade.bottom_right + (80, 80), 64, self.game, self)
