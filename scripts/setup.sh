863376ddccd4489a214187dc385d557a71f96f10
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-23 22:55:43 +0100

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,8 @@ set -e
 
 cd "$(dirname "$0")/.." || exit 1
 
+rm -rf ./venv
+
 git submodule update --init
 
 python3 -m venv ./venv
@@ -13,8 +15,14 @@ source ./venv/bin/activate
 pip install -r ./requirements.txt
 pip install -r ./requirements-dev.txt
 
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:inkscape.dev/stable
+sudo add-apt-repository universe || {
+  echo "Failed to get repository"
+}
+
+sudo add-apt-repository ppa:inkscape.dev/stable || {
+  echo "Failed to get repository"
+}
+
 sudo apt-get update
 sudo apt install inkscape
 
