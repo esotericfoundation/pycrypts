5afccef98b6baeaeefbab802b7f16ced83222450
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-23 14:33:22 +0100

    Add a few candles to Bozo's room

--- pycrypts/rooms/bozo_boss_barrack.py
+++ pycrypts/rooms/bozo_boss_barrack.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 from pygame import Vector2
 
 from .room import Room
+from ..tickable.renderable.collidable.entities.candle import Candle
 from ..tickable.renderable.collidable.entities.living.monsters.zombie import Zombie
 from ..tickable.renderable.collidable.walls.brittle_wall import BrittleWall
 from ..tickable.renderable.collidable.walls.door import Door
@@ -32,11 +33,23 @@ class BozoBossBarrack(Room):
         bozo_boss_barracks_barricade = Door((0, 280), (40, 440), self.game.entrance_zone, Vector2(80, 360), self.game, self)
 
         top_left_corner_block = Wall((40, 40), (80, 80), self.game, self, True)
+
+        Candle(self.game, self, top_left_corner_block.bottom_right + (40, 40))
+
         next_top_corner = Wall(top_left_corner_block.top_left + (320, 0), top_left_corner_block.bottom_right + (320, 0), self.game, self, True)
+
+        Candle(self.game, self, next_top_corner.bottom_right + (-120, 40))
+
         Wall(top_left_corner_block.top_left + (440, 0), top_left_corner_block.bottom_right + (440, 0), self.game, self, True)
         Wall(top_left_corner_block.top_left + (1160, 0), top_left_corner_block.bottom_right + (1160, 0), self.game, self, True)
         bottom_left_corner_block = Wall((40, 640), (80, 680), self.game, self, True)
+
+        Candle(self.game, self, bottom_left_corner_block.get_top_right() + (40, -80))
+
         next_bottom_corner = Wall(bottom_left_corner_block.top_left + (320, 0), bottom_left_corner_block.bottom_right + (320, 0), self.game, self, True)
+
+        Candle(self.game, self, next_bottom_corner.get_top_right() + (-120, -80))
+
         Wall(bottom_left_corner_block.top_left + (440, 0), bottom_left_corner_block.bottom_right + (440, 0), self.game, self, True)
         Wall(bottom_left_corner_block.top_left + (1160, 0), bottom_left_corner_block.bottom_right + (1160, 0), self.game, self, True)
 
---
+++ pycrypts/tickable/renderable/collidable/entities/candle.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from pygame import Vector2
+
+from pycrypts.rooms.room import Room
+from pycrypts.tickable.renderable.collidable.entities.entity import Entity
+
+if TYPE_CHECKING:
+    from pycrypts.game import PyCrypts
+
+
+class Candle(Entity):
+
+    def __init__(self, game: "PyCrypts", room: "Room", position: tuple[int, int] | Vector2):
+        super().__init__(game, room, position, "lantern", 64)
+
+        self.no_clip = True
+
+    def tick(self):
+        self.render()
+        self.render_light(600)
