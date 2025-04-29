cc1b94a85d033ea9d75a5ea0af2dd8bfdbf71e41
rolyPolyVole <yezhanting@gmail.com>
2025-04-29 18:26:34 +0100

    Improve candle positioning

--- pycrypts/rooms/bozo_boss_barrack.py
+++ pycrypts/rooms/bozo_boss_barrack.py
@@ -40,12 +40,10 @@ class BozoBossBarrack(Room):
         Wall(top_border.bottom_right + (-40, 0), bottom_border.get_top_right() + (-50, 0), self.game, self, True)
 
         top_left_corner_block = Wall((40, 40), (80, 80), self.game, self, True)
-
-        Candle(self.game, self, top_left_corner_block.bottom_right + (40, 40))
-
         next_top_corner = Wall(top_left_corner_block.top_left + (320, 0), top_left_corner_block.bottom_right + (320, 0), self.game, self, True)
 
-        Candle(self.game, self, next_top_corner.bottom_right + (-120, 40))
+        Candle(self.game, self, top_left_corner_block.bottom_right + (0, -40))
+        Candle(self.game, self, next_top_corner.bottom_right + (-80 + 8, -40))
 
         big_top_stub_right = Wall(top_left_corner_block.top_left + (440, 0), top_left_corner_block.bottom_right + (440, 0), self.game, self, True)
         top_right_corner_block = Wall(top_left_corner_block.top_left + (1160, 0), top_left_corner_block.bottom_right + (1160, 0), self.game, self, True)
@@ -54,14 +52,14 @@ class BozoBossBarrack(Room):
         Wall(big_top_stub_right.bottom_right + (80, 120), big_top_stub_right.bottom_right + (120, 160), self.game, self)
         Wall(top_right_corner_block.get_bottom_left() + (-120, 120), top_right_corner_block.get_bottom_left() + (-80, 160), self.game, self)
 
-        Candle(self.game, self, big_top_stub_right.bottom_right + (40, 40))
-        Candle(self.game, self, top_right_corner_block.get_bottom_left() + (-80, 40))
+        Candle(self.game, self, big_top_stub_right.bottom_right + (0, -40))
+        Candle(self.game, self, top_right_corner_block.get_bottom_left() + (-40 + 8, -40))
 
         bottom_left_corner_block = Wall((40, 640), (80, 680), self.game, self, True)
         next_bottom_corner = Wall(bottom_left_corner_block.top_left + (320, 0), bottom_left_corner_block.bottom_right + (320, 0), self.game, self, True)
 
-        Candle(self.game, self, bottom_left_corner_block.get_top_right() + (40, -80))
-        Candle(self.game, self, next_bottom_corner.get_top_right() + (-120, -80))
+        Candle(self.game, self, bottom_left_corner_block.get_top_right() + (0, -0))
+        Candle(self.game, self, next_bottom_corner.get_top_right() + (-80 + 8, -0))
 
         big_bottom_stub_right = Wall(bottom_left_corner_block.top_left + (440, 0), bottom_left_corner_block.bottom_right + (440, 0), self.game, self, True)
         bottom_right_corner_block = Wall(bottom_left_corner_block.top_left + (1160, 0), bottom_left_corner_block.bottom_right + (1160, 0), self.game, self, True)
@@ -70,8 +68,8 @@ class BozoBossBarrack(Room):
         Wall(big_bottom_stub_right.get_top_right() + (80, -160), big_bottom_stub_right.get_top_right() + (120, -120), self.game, self)
         Wall(bottom_right_corner_block.top_left + (-120, -160), bottom_right_corner_block.top_left + (-80, -120), self.game, self)
 
-        Candle(self.game, self, big_bottom_stub_right.get_top_right() + (40, -80))
-        Candle(self.game, self, bottom_right_corner_block.top_left + (-80, -80))
+        Candle(self.game, self, big_bottom_stub_right.get_top_right() + (0, 0))
+        Candle(self.game, self, bottom_right_corner_block.top_left + (-40 + 8, 0))
 
         stub_1 = Wall(next_top_corner.top_left + (40, 0), next_top_corner.bottom_right + (80, 40), self.game, self, True)
         stub_2 = Wall(next_bottom_corner.top_left + (40, -40), next_bottom_corner.bottom_right + (80, 0), self.game, self, True)
