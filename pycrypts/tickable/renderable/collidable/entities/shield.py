55e9bb7f1c1f37a437e8a3049dc45b18af07f877
rolyPolyVole <yezhanting@gmail.com>
2025-04-29 23:33:55 +0100

    Rename variables

--- pycrypts/tickable/renderable/collidable/entities/shield.py
+++ pycrypts/tickable/renderable/collidable/entities/shield.py
@@ -9,14 +9,13 @@ from ..collidable import Collidable
 if TYPE_CHECKING:
     from .....game import PyCrypts
     from .....rooms.room import Room
-    from .living.monsters.monster import Monster
 
 
 class Shield(Entity):
-    def __init__(self, monster: "Monster", game: "PyCrypts", room: "Room"):
-        super().__init__(game, room, monster.position, "shield", 64)
+    def __init__(self, user: "Entity", game: "PyCrypts", room: "Room"):
+        super().__init__(game, room, user.position, "shield", 64)
 
-        self.monster = monster
+        self.user = user
         self.target = None
         self.no_clip = True
         self.block_sound = game.get_sound("shield_block")
@@ -24,16 +23,16 @@ class Shield(Entity):
     def tick(self):
         super().tick()
 
-        if not self.monster.is_valid():
+        if not self.user.is_valid():
             self.unload()
 
-        self.position = self.monster.position + (self.monster.size * 2.0 / 5.0, self.monster.size * 2.0 / 5.0)
+        self.position = self.user.position + (self.user.size * 2.0 / 5.0, self.user.size * 2.0 / 5.0)
 
     def is_colliding(self, entity: Collidable) -> bool:
         if not isinstance(entity, Projectile):
             return False
 
-        if isinstance(entity.shooter, type(self.monster)):
+        if isinstance(entity.shooter, type(self.user)):
             return False
 
         if self.position.distance_squared_to(entity.position) < ((self.size / 2 + entity.size / 2) ** 2):
